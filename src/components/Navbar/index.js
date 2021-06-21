6769302bb1ae6bf3c365cfa84ad83beed53824ee
TheoB-png <theo.beloeil.pro@gmail.com>
2021-06-21 21:01:47 +0200

    add hidden link on home navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import scrollToTop from 'src/hooks/scrollToTop';
 import './styles.scss';
@@ -7,36 +7,44 @@ const Navbar = ({
   scroll,
   scrollWork,
   scrollCredits,
-}) => (
-  <nav className="navbar">
-    <ul className="navbar__container">
-      <li
-        className="navbar__link"
-        onClick={() => scrollToTop()}
-      >
-        Home
-      </li>
-      <li
-        className="navbar__link"
-        onClick={() => scroll()}
-      >
-        About
-      </li>
-      <li
-        className="navbar__link"
-        onClick={() => scrollWork()}
-      >
-        Work
-      </li>
-      <li
-        className="navbar__link"
-        onClick={() => scrollCredits()}
-      >
-        Credits
-      </li>
-    </ul>
-  </nav>
-);
+}) => {
+  const [surprise, setSurprise] = useState(false);
+  const handleEasterEgg = () => setSurprise(!surprise);
+  return (
+    <nav className="navbar">
+      <ul className="navbar__container">
+        <a className={surprise ? 'navbar__surprise navbar__surprise--on' : 'navbar__surprise'} href="https://www.youtube.com/watch?v=sfCvf8pEj_Y" target="_blank" rel="noreferrer">You're already home</a>
+        <li
+          className="navbar__link"
+          onClick={() => {
+            scrollToTop();
+            handleEasterEgg();
+          }}
+        >
+          Home
+        </li>
+        <li
+          className="navbar__link"
+          onClick={() => scroll()}
+        >
+          About
+        </li>
+        <li
+          className="navbar__link"
+          onClick={() => scrollWork()}
+        >
+          Work
+        </li>
+        <li
+          className="navbar__link"
+          onClick={() => scrollCredits()}
+        >
+          Credits
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 Navbar.propTypes = {
   scroll: PropTypes.func.isRequired,

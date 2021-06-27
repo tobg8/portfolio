8bd30f7e18f1456253f9733d476b2010c45be249
TheoB-png <theo.beloeil.pro@gmail.com>
2021-06-27 19:06:41 +0200

    modify data

--- src/assets/data/project.js
+++ src/assets/data/project.js
@@ -2,21 +2,21 @@ export default [
   {
     id: 1,
     name: 'Où est mon pain',
-    description: 'Bakery e-commerce website, we were 4 on this i worked on the front aspect, react-redux.',
+    description: 'Bakery e-commerce website, we were 4 on this i worked on the front aspect, React-Redux.',
     websiteUrl: 'http://ou-est-mon-pain.surge.sh/',
     githubUrl: '',
   },
   {
     id: 2,
     name: 'Sorare bet',
-    description: 'A website where you could use your Sorare NFT cards to compete in leagues each week. Sequelize-Psql, Express, graphQL, React-Redux.',
+    description: 'A website where you could use your Sorare NFT cards to compete in leagues each week. Sequelize-Psql, Express, GraphQL, React-Redux.',
     websiteUrl: 'https://sorare-league.surge.sh/',
     githubUrl: 'https://github.com/tobg8/sorare-bet',
   },
   {
     id: 3,
     name: 'Bauhaus 2.0',
-    description: 'Found this website on behance and tried to reproduce it ... not a complete sucess on desktop version but it\'s ok i guess.',
+    description: 'Found this website on behance and tried to reproduce it ... not a complete sucess on desktop but it\'s ok i guess. React only.',
     websiteUrl: 'http://test-grid.surge.sh/',
     githubUrl: 'https://github.com/tobg8/grid-design',
   },
@@ -30,7 +30,7 @@ export default [
   {
     id: 5,
     name: 'This actual portfolio',
-    description: 'Well you looking at it, hope it looks well on every devices, i\'m sure it doesn\'t. React only, did not wanted to use a state manager this time.',
+    description: 'Well you looking at it, hope it looks well on every devices. React only, did not wanted to use a state manager this time.',
     websiteUrl: '',
     githubUrl: 'https://github.com/tobg8/portfolio',
   },
